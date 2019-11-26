6c8542a8bccfad7d35b6deeca4ff835ef13e74eb
emsw.bandara <emsw.bandara@gmail.com>
2019-11-26 12:21:31 +0530

    Add configuration properties required for REST APIs

--- components/org.wso2.carbon.identity.provisioning.connector.salesforce/src/main/java/org/wso2/carbon/identity/provisioning/connector/salesforce/SalesforceProvisioningConnectorFactory.java
+++ components/org.wso2.carbon.identity.provisioning.connector.salesforce/src/main/java/org/wso2/carbon/identity/provisioning/connector/salesforce/SalesforceProvisioningConnectorFactory.java
@@ -25,6 +25,9 @@ import org.wso2.carbon.identity.provisioning.AbstractOutboundProvisioningConnect
 import org.wso2.carbon.identity.provisioning.AbstractProvisioningConnectorFactory;
 import org.wso2.carbon.identity.provisioning.IdentityProvisioningException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SalesforceProvisioningConnectorFactory extends AbstractProvisioningConnectorFactory {
 
     private static final Log log = LogFactory.getLog(SalesforceProvisioningConnectorFactory.class);
@@ -48,4 +51,103 @@ public class SalesforceProvisioningConnectorFactory extends AbstractProvisioning
         return SALESFORCE;
     }
 
+    /**
+     * Get Configuration Properties.
+     */
+    @Override
+    public List<Property> getConfigurationProperties() {
+
+        List<Property> configProperties = new ArrayList<>();
+        Property apiVersion = new Property();
+        apiVersion.setName(SalesforceConnectorConstants.PropertyConfig.API_VERSION);
+        apiVersion.setDisplayName("API version");
+        apiVersion.setRequired(true);
+        apiVersion.setType("string");
+        apiVersion.setDisplayOrder(1);
+        configProperties.add(apiVersion);
+
+        Property domain = new Property();
+        domain.setName(SalesforceConnectorConstants.PropertyConfig.DOMAIN_NAME);
+        domain.setDisplayName("Domain Name");
+        domain.setRequired(true);
+        domain.setType("string");
+        domain.setDisplayOrder(2);
+        configProperties.add(domain);
+
+        Property clientId = new Property();
+        clientId.setName(SalesforceConnectorConstants.PropertyConfig.CLIENT_ID);
+        clientId.setDisplayName("Client ID");
+        clientId.setRequired(true);
+        clientId.setType("string");
+        clientId.setDisplayOrder(3);
+        configProperties.add(clientId);
+
+        Property clientSecret = new Property();
+        clientSecret.setName(SalesforceConnectorConstants.PropertyConfig.CLIENT_SECRET);
+        clientSecret.setDisplayName("Client Secret");
+        clientSecret.setRequired(true);
+        clientSecret.setType("string");
+        clientSecret.setDisplayOrder(4);
+        configProperties.add(clientSecret);
+
+        Property username = new Property();
+        username.setName(SalesforceConnectorConstants.PropertyConfig.USERNAME);
+        username.setDisplayName("Username");
+        username.setRequired(true);
+        username.setType("string");
+        username.setDisplayOrder(5);
+        configProperties.add(username);
+
+        Property password = new Property();
+        password.setName(SalesforceConnectorConstants.PropertyConfig.PASSWORD);
+        password.setDisplayName("Password");
+        password.setRequired(true);
+        password.setDescription("Enable User password provisioning to a SCIM domain");
+        password.setType("boolean");
+        password.setDefaultValue("true");
+        password.setDisplayOrder(6);
+        configProperties.add(password);
+
+        Property tokenEp = new Property();
+        tokenEp.setName(SalesforceConnectorConstants.PropertyConfig.OAUTH2_TOKEN_ENDPOINT);
+        tokenEp.setDisplayName("OAuth2 Token Endpoint");
+        tokenEp.setRequired(true);
+        tokenEp.setType("string");
+        tokenEp.setDefaultValue("https://login.salesforce.com/services/oauth2/token");
+        tokenEp.setDisplayOrder(7);
+        configProperties.add(tokenEp);
+
+        Property provPattern = new Property();
+        provPattern.setName(SalesforceConnectorConstants.PropertyConfig.PROVISIONING_PATTERN_KEY);
+        provPattern.setDisplayName("Provisioning Pattern");
+        provPattern.setRequired(false);
+        provPattern.setDescription("This pattern is used to build the user id of Salesforce domain. Combination of " +
+                "attributes UD (User Domain), UN (Username), TD (Tenant Domain) and IDP (Identity Provider) can be " +
+                "used to construct a valid pattern. Ex: {UD, UN, TD, IDP}");
+        provPattern.setType("string");
+        provPattern.setDisplayOrder(8);
+        configProperties.add(provPattern);
+
+        Property provSeperator = new Property();
+        provSeperator.setName(SalesforceConnectorConstants.PropertyConfig.PROVISIONING_SEPERATOR_KEY);
+        provSeperator.setDisplayName("Provisioning Separator");
+        provSeperator.setRequired(false);
+        provSeperator.setDescription("This is the separator of attributes in Salesforce Outbound Provisioning pattern" +
+                ". For example if pattern is {UN,TD} and Username: testUser, Tenant Domain: TestTenant.com, " +
+                "Separator:_, Google Domain : testmail.com then the privisioining email is testUser_testTenant" +
+                ".com@testmail.com");
+        provSeperator.setType("string");
+        provSeperator.setDisplayOrder(9);
+        configProperties.add(provSeperator);
+
+        Property provDomain = new Property();
+        provDomain.setName(SalesforceConnectorConstants.PropertyConfig.PROVISIONING_DOMAIN_KEY);
+        provDomain.setDisplayName("Provisioning Domain");
+        provDomain.setRequired(false);
+        provDomain.setType("string");
+        provDomain.setDisplayOrder(10);
+        configProperties.add(provDomain);
+
+        return configProperties;
+    }
 }
